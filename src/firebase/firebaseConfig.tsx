1c508c29cef4958a1524dc3a25dcf548ebd24a5c
Mishael Enyi <enyimishael22@gmail.com>
2026-09-24 15:45:49 +0100

    Update Firebase config to use only environment variables
    Removed default values for Firebase configuration and set them to use environment variables directly.

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyB6pHje3a6b_TL5QGVIiZ-9m-73IsjE6cs",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "cuttr-c1515.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "cuttr-c1515",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "cuttr-c1515.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "179583977080",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:179583977080:web:7a52382ef91964690a4f8d",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-9RBDMNLCRQ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
